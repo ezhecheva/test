44377e94bc4553c5c229b5c5ccc70e33fd153ec3
ezhecheva <100564100+ezhecheva@users.noreply.github.com>
2022-03-07 11:30:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 class MyMath:
     def __init__(self) -> None:
         pass
+
+    def factorial(self, x: int):
+        f = 1
+
+        for number in range(1, x + 1):
+            f *= number
+
+        return f
